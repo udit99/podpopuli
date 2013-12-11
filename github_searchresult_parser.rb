6550029b08890af601b128beccb037e451dc1d5a
Udit Manektala <udit@involvecare.com>
2013-12-10 19:10:45 -0800

    moving from class methods to instance methods

--- github_searchresult_parser.rb
+++ github_searchresult_parser.rb
@@ -4,6 +4,6 @@ class GithubSearchresultParser
   def self.parse_podnames(url)
       doc = Nokogiri::HTML(open(url))
       podfile_lines = doc.css(".line").map(&:text).map(&:chomp).uniq
-      PodfileParser.parse_podnames(podfile_lines)
+      PodfileParser.new.parse_podnames(podfile_lines)
   end
 end
--- podfile_parser.rb
+++ podfile_parser.rb
@@ -1,5 +1,5 @@
 class PodfileParser
-  def self.parse_podnames(podfile_lines)
+  def parse_podnames(podfile_lines)
     podfile_lines.map do |line|
       line.match(/^pod/) ? scrub(line) :nil
     end.compact
@@ -7,13 +7,13 @@ class PodfileParser
 
   private
 
-  def self.scrub(line)
+  def scrub(line)
     line = line.gsub(/^pod/, '').strip
     line = line.split(",").first
     strip_quotes(line)
   end
 
-  def self.strip_quotes(str)
+  def strip_quotes(str)
     str.gsub(/(\'|\")/,'')
   end
 end
